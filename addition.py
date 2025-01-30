9d046fc3e812c14e5ad516d01e352c2c84e7a94a
V KishorPatil <v.kishorpatil@adfactorspr.local>
2025-01-30 16:52:16 +0530

    add messgae

--- addition.py
+++ addition.py
@@ -4,6 +4,8 @@ c = a+b
 d = a*b
 e = a-b
 
+print("enter the number of digit:-")
+
 print("total number of additon:-", c)
 print("total number of multification:-", d)
 print("total number of subtraction:-", e)
